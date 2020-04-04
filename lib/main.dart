f6948d90fe3645cef55c15fb1c9e24e2a2694ea4
Yevgeniy Malyshev <meugen.82@gmail.com>
2020-04-04 14:24:34 +0300

    Implemented magic ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MainPage(),
       ),
     );
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: BallPage(),
+      ),
+    );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var random = Random();
+  var ballIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballIndex = random.nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballIndex.png'),
+      ),
+    );
+  }
+}
